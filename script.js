f61b5f2e80b35fe6f38ed6797733f1fedc90e203
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-02-18 09:41:06 -0600

    made a split text work

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var wordArray = [];
-var word = document.querySelector('p1word')
-console.log(p1word)
+var wordSize = 3; //maximum word length
 
 
 document.addEventListener('click', (event) => {
-    console.log (eventListener)
     event.preventDefault()
-console.log(preventDefault)
+
 })
+
+// Split the text entry word into an array
+var word = "dog";
+var wordSplit = word.split("");
+console.log (wordSplit);
+
+
+
+// for (let i=0; i < size; i++){
+//     wordArray[i] = ('word' + (i+1));
+// }
