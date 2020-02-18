7216262b2f95aec65377c0b1a80a685637eb8e8f
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-02-18 08:24:18 -0600

    js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var wordArray = [];
+var word = document.querySelector('p1word')
+console.log(p1word)
 
 
-
+document.addEventListener('click', (event) => {
+    console.log (eventListener)
+    event.preventDefault()
+console.log(preventDefault)
+})
