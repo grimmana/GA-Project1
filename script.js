8828ceb993ea3ef520aeb2d381f20f3f327c7e64
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-02-18 19:37:20 -0600

    working on storing array data

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let wordSize = 20; //maximum word length 
-let wordChoiceArray = [];
-let letterChoiceArray = [];
+let wordChoiceArrayX = [];
+let letterChoiceArrayX = [];
 // player 1
 document.addEventListener('submit', (event) => {
     event.preventDefault();  
 // split the text field value    
 let word = document.getElementById("p1word").value.split("");
-//console.log(word); // displays the array as expected
+// console.log(word); // displays the array as expected
 // create a new array
-let wordChoiceArray = []
+//let wordChoiceArray = []
+//let wordChoiceArrayX = word
 // assign the content to the new array
-wordChoiceArray.push(word);
-//console.log(wordChoiceArray);
+// wordChoiceArray.push(word);
+wordChoiceArrayX.push(word)
+console.log(wordChoiceArrayX);
 //reset the word input field    
-//document.getElementById("p1word").value = "";
+document.getElementById("p1word").value = "";
+// Reset the guess letter field    
+// document.getElementById("p2letter").value = "";
 
 })
 // player 2
@@ -22,16 +26,17 @@ document.addEventListener('submit', (event) => {
 // get the text field value
 let guess = document.getElementById("p2letter").value;
 // create a new array
-let letterChoiceArray = []
+//let letterChoiceArray = []
 // assign the content to a new array
-letterChoiceArray.push(guess);
+letterChoiceArrayX.push(guess);
+console.log(letterChoiceArrayX);
 //console.log(letterChoiceArray);
 // Reset the guess letter field    
 document.getElementById("p2letter").value = "";
 // check the guess against the wordChoiceArray  
-let wordChoiceArray = document.getElementById("p1word").value.split("");
-console.log(letterChoiceArray);
-console.log(wordChoiceArray);
+// let wordChoiceArray = document.getElementById("p1word").value.split("");
+//console.log(letterChoiceArray);
+//console.log(wordChoiceArray);
 
 // run a compare
 
