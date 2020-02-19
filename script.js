a445dd4387eb9b2f92f3f3bf41cf39859bea1744
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-02-19 09:18:37 -0600

    added grid element

--- script.js
+++ script.js
@@ -8,6 +8,24 @@ let word = document.getElementById("p1word").value.split("");
 wordChoiceArrayX.push(word);
 document.getElementById("p1word").value = "";
 });
+//////////////////////
+// const gridArray =
+//   [[`f`, `r`, `e`, `a`, `k`, `i`,`s`, `h`]]
+   
+let gridTemplate = ``
+   
+wordChoiceArrayX.forEach(function(p1word) {
+	gridTemplate += `"${p1word}"`
+})
+
+while(gridTemplate.includes(`,`)) {
+	gridTemplate = gridTemplate.replace(`,` , ` `)
+}
+
+document.querySelector(`p`).textContent = `\'This is the gridTemplate: ${gridTemplate}.\'`
+document.querySelector(`p`).style.gridTemplateAreas = gridTemplate;
+
+///////////////////////////////
 // player 2
 document.addEventListener('submit', (event) => {
     event.preventDefault();
