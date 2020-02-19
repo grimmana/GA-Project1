599a8d8735e9566d26bf709b020174d8dba863c2
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-02-18 21:51:57 -0600

    ran through css html and js checkers

--- script.js
+++ script.js
@@ -1,45 +1,17 @@
-let wordSize = 20; //maximum word length 
 let wordChoiceArrayX = [];
 let letterChoiceArrayX = [];
 
 // player 1
 document.addEventListener('submit', (event) => {
     event.preventDefault();  
-// split the text field value    
 let word = document.getElementById("p1word").value.split("");
-// assign the content to the new array
 wordChoiceArrayX.push(word);
-console.log(wordChoiceArrayX);
-//reset the word input field    
 document.getElementById("p1word").value = "";
-})
+});
 // player 2
 document.addEventListener('submit', (event) => {
     event.preventDefault();
-// get the text field value
 let guess = document.getElementById("p2letter").value;
-// assign the content to a new array
-letterChoiceArrayX.push(guess);
-console.log(letterChoiceArrayX);
-// reset the guess letter field    
+letterChoiceArrayX.push(guess); 
 document.getElementById("p2letter").value = "";
-})
-
-
-
-// check the guess against the wordChoiceArray  
-// let wordChoiceArray = document.getElementById("p1word").value.split("");
-//console.log(letterChoiceArray);
-//console.log(wordChoiceArray);
-
-// run a compare
-
-// wordChoiceArray.includes(letterChoiceArray);
-// return true; return false;
-
-
-//function findInArray(letterChoiceArray) {
-//for (let i=0; i < size; i++){
-//letterChoiceArray[i] = ('wordChoiceArray' + (i+1));
-//return true || false
-//}}})
+});
