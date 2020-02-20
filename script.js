40c12e994425197e3158931c72770a173d4d3463
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-02-20 07:56:57 -0600

    lots of changes; no progress made

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-let wordChoiceArray = [];
-let letterChoiceArray = [];
-
+let wordArray = [];
+let letterArray = [];
 // player 1
 document.addEventListener('submit', (event) => {
     event.preventDefault();  
 let word = document.getElementById("p1word").value.split("");
-wordChoiceArray.push(word);
-//clear field // document.getElementById("p1word").value = "";
+wordArray.push(word);
+//document.getElementById("p1word").value = "";  //clear field
+//console.log(wordArray);
 });
 // player 2
 document.addEventListener('submit', (event) => {
     event.preventDefault();
-let guess = document.getElementById("p2letter").value;
-letterChoiceArray.push(guess); 
-//clear field // document.getElementById("p2letter").value = "";
+let letter = document.getElementById("p2letter").value;
+letterArray.push(letter); 
+//document.getElementById("p2letter").value = "";  //clear field
+//console.log(letterArray);
+//letterArray.shift();
 });
-
-
-
-
-
 
 
