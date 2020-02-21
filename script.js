eb30d5f66fd14b64c8004b03e06c90e91518b7b2
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-02-21 09:29:17 -0600

    friday morning 9:288 during fixes

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 
 //Input entry player 1
-function player1() {
+
+function player1(reset) {
         document.getElementById("form1").addEventListener('submit', (event) => {
         event.preventDefault();
         let word = document.getElementById("p1word").value.split("");
         console.log(word);
+        alert("Word is" + " " + (word.length) + " " + "characters");
         // for(let i=0; i<=word.length; i++){
         //         word[i] + "<br>";
         //         console.log(word[i]);
-       // document.getElementById("p1word").value = " ";  
-    //alert("Word is" + " " + (word.length) + " " + "characters");
+   //    document.getElementById("p1word").value = " ";  
+//alert("Word is" + " " + (wordS.length) + " " + "characters");
     //}
 });
 }
 player1();
+function reset(){
+        document.getElementById("p1word").value = "";  
+      }
+reset();
+
+
+
+let word = [];
+console
+let letter = [];
+
+
+//        for(let i=0; i<=word.length; i++){
+//                 word[i] + "<br>";
+//                 console.log(word[i]);
+//               }
+//Search box input
+
 
 //Input entry player 2
 function player2() {
@@ -28,6 +48,8 @@ function player2() {
       //  document.getElementById("p2word").value = " ";  
    // }
 
+
+//
 //https://www.the-art-of-web.com/javascript/validate/
    // count tries
 
@@ -45,24 +67,26 @@ function player2() {
 player2();
 
 
-let guess1 = [];
-let guess2 = [];
-let guess3 = [];
-let guess4 = [];
-let guess5 = [];
+// let guess1 = [];
+// let guess2 = [];
+// let guess3 = [];
+// let guess4 = [];
+// let guess5 = [];
 
-let letter = [];
-let word = []
+// let letter = [];
+
 
     
-function checkGuess(){
-for(let i=0; i<=word.length; i++){
-        word[i] + "<br>";
-        console.log(word[i]);
-        for(let i=0; i<=letter.length; i++){
-                letter[i] + "<br>";
-                console.log(letter[i]);
-        }}}
+// function checkGuess(){
+// for(let i=0; i<=word.length; i++){
+//         word[i] + "<br>";
+//         console.log(word[i]);
+//         for(let i=0; i<=letter.length; i++){
+//                 letter[i] + "<br>";
+//                 console.log(letter[i]);
+//         }}}
 
-
+// function reset(){
+//   document.getElementById("p2word").value = " ";  
+// }
 
